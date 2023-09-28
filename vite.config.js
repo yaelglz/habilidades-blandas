25ea048ab472cfd3252a156f0e2701e614c052ac
yaelglz <yaelglzmunoz@gmail.com>
2023-09-27 21:17:55 -0600

    Update

--- vite.config.js
+++ vite.config.js
@@ -8,7 +8,7 @@ export default defineConfig({
   plugins: [
     vue(),
   ],
-  base: "/habilidades-blandas/",
+  // base: "/habilidades-blandas/",
   resolve: {
     alias: {
       '@': fileURLToPath(new URL('./src', import.meta.url))
